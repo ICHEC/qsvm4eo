49cd64c2497446e33bba738551dfcce6ec565373
James Nelson <ja94nelson@gmail.com>
2025-11-25 09:54:38 +0000

    refactor mpi

--- mpi_workflow/qsvm_train.py
+++ mpi_workflow/qsvm_train.py
@@ -4,24 +4,29 @@ from mpi4py import MPI
 from qlmaas.qpus import AnalogQPU
 from qat.core import Schedule
 
+# Get the parent node
 comm = MPI.Comm.Get_parent()
 
+# Receive the qubit geometries
 qubit_coords = comm.recv(source=0, tag=0)
 
-drive = qsvm4eo.generate_rydberg_hamiltonian(qubit_coords)
-
-duration = 660  # ns
-duration /= 1000
-schedule = Schedule(drive=drive, tmax=duration)
-
+# Create jobs
+schedule = Schedule(
+    drive=qsvm4eo.generate_rydberg_hamiltonian(qubit_coords), tmax=0.66  # μs
+)
 job = schedule.to_job()
 
+# Run jobs
 my_qpu = AnalogQPU()
 async_result = my_qpu.submit(job)
 result = async_result.join()
 
+# Get state probabilities
 probs = np.array([r.probability for r in result])
+
+# Send state probabilities
 comm.send(probs, dest=0, tag=1)
 
+# Send end signal
 comm.send(1, dest=0, tag=2)
 comm.Disconnect()
--- mpi_workflow/run_workflow.py
+++ mpi_workflow/run_workflow.py
@@ -2,13 +2,13 @@ import qsvm4eo
 import numpy as np
 from mpi4py import MPI
 
-# load data
+# Load the data
 num_features = 4
 x_train, y_train, x_test, y_test = qsvm4eo.load_data(
     data_path="..", num_features=num_features, scale_features=False
 )
 
-# encode the data
+# Encode the data, transforming the features into qubit coordinates
 encoding = qsvm4eo.RadialEncoding(
     max_feature=np.max(x_train), shift=1.0, scaling=5.4, n_features=num_features
 )
@@ -17,14 +17,18 @@ qbits0 = encoding.encode(x_train[0])
 print("Qubit Geometry:")
 print(qbits0)
 
+# Create a worker for the quantum task
+worker = MPI.COMM_SELF.Spawn("python", "./qsvm_train.py", 1)
 
-worker = MPI.COMM_SELF.Spawn("python", "./quantum_task.py", 1)
+# Send the qubit geometries
 worker.send(qbits0, dest=0, tag=0)
 
+# Recieve the state probabilities.
 probs = worker.recv(source=0, tag=1)
 print("Probabilities:")
 print(probs)
 
+# Recieve the end signal
 end = worker.recv(source=0, tag=2)
 print("Master end signal received, end the job!")
 
