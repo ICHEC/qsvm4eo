a44d55115d2704b2a3c0fde0717ddc1d035d940a
James Nelson <ja94nelson@gmail.com>
2025-11-25 10:48:51 +0000

    full workflow

--- mpi_workflow/run_workflow.py
+++ mpi_workflow/run_workflow.py
@@ -3,6 +3,29 @@ import numpy as np
 from mpi4py import MPI
 from sklearn.svm import SVC
 
+
+def compute_probabilities(qbits):
+    # Create a worker for the quantum task
+    worker = MPI.COMM_SELF.Spawn("python", "./qsvm_train.py", 1)
+
+    # Send the qubit geometries
+    print("Computing probabilities")
+    worker.send(qbits, dest=0, tag=0)
+
+    # Recieve the state probabilities.
+    probs = worker.recv(source=0, tag=1)
+    print("Finished computing probabilities:")
+    print(probs_train.shape)
+
+    # Recieve the end signal
+    end = worker.recv(source=0, tag=2)
+    print("Master end signal received, end the job!")
+
+    # disconnect from worker
+    worker.Disconnect()
+    return probs
+
+
 # Load the data
 num_features = 4
 x_train, y_train, x_test, y_test = qsvm4eo.load_data(
@@ -13,36 +36,30 @@ x_train, y_train, x_test, y_test = qsvm4eo.load_data(
 encoding = qsvm4eo.RadialEncoding(
     max_feature=np.max(x_train), shift=1.0, scaling=5.4, n_features=num_features
 )
-qbits_train = [encoding.encode(x) for x in x_train[:50]]
+qbits_train = [encoding.encode(x) for x in x_train]
+qbits_test = [encoding.encode(x) for x in x_test]
 
 print("Qubit Geometries:")
 print(qbits_train[:4])
 
-# Create a worker for the quantum task
-worker = MPI.COMM_SELF.Spawn("python", "./qsvm_train.py", 1)
-
-# Send the qubit geometries
-worker.send(qbits_train, dest=0, tag=0)
-
-# Recieve the state probabilities.
-probs_train = worker.recv(source=0, tag=1)
-print("Probabilities:")
-print(probs_train.shape)
-
-# Recieve the end signal
-end = worker.recv(source=0, tag=2)
-print("Master end signal received, end the job!")
-
-# disconnect from worker
-worker.Disconnect()
+print("Training:")
+probs_train = compute_probabilities(qbits_train)
 
 # Compute the kernel
 kernel = qsvm4eo.Kernel()
 gram_train = kernel.compute_gram_train(probs_train)
 
-# Fit the SVM
+# Fit the SVM ad get the score
 model = SVC(kernel="precomputed")
-model.fit(gram_train, y_train[:50])
+model.fit(gram_train, y_train)
+train_score = model.score(gram_train, y_train)
 
-train_score = model.score(gram_train, y_train[:50])
+print("Testing")
+probs_test = compute_probabilities(qbits_test)
+
+# Compute the kernel and score
+gram_test = kernel.compute_gram_train(probs_test)
+test_score = model.score(gram_test, y_test)
+
 print("Train acc:", train_score)
+print("Test acc:", test_score)
